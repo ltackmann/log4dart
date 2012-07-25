192127513ff7af57befeacd4742722edc40de6ea
Lars Tackmann <ltackmann@gmail.com>
2012-07-24 17:21:42 -0700

    rename to Solvr

---
+++ lib.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2012 Qalqo, all rights reserved.  
+//
+// This open source software is governed by the license terms 
+// specified in the LICENSE file
+
+#library('qalqo:log4dart');
+
+#source('appender.dart');
+#source('console_appender.dart');
+#source('logger.dart');
+#source('logger_impl.dart');
+#source('logger_factory.dart');
+
---
+++ test/test_runner.dart
@@ -0,0 +1,25 @@
+#library("qalqo:common:log4dart:test");
+
+#import('../lib.dart');
+#import('../file/file_appender.dart');
+
+#source('context_log_test.dart');
+#source('simple_log_test.dart');
+
+main() {
+  // simple logger setup
+  LoggerFactory.builder = (name) => new LoggerImpl(name, infoEnabled:false); 
+  
+  // Advanced logger setup
+  LoggerFactory.builder = (name) {
+    Map<String,Logger> loggerMap = {
+      "SimpleLogTest": new LoggerImpl(name, debugEnabled:false, infoEnabled:false),
+      "ContextLogTest": new LoggerImpl(name, debugEnabled:true, appenders:[new FileAppender("/tmp/log.txt")])
+    };
+    return loggerMap[name];
+  }; 
+  
+  new SimpleLogTest();
+  new ContextLogTest();
+}
+
