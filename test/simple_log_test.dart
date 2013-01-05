8f220502f31bf25df9b3f6c6a1c42f761fbb881f
Lars Tackmann <ltackmann@gmail.com>
2013-01-05 19:07:55 +0100

    fix copyright

--- test/simple_log_test.dart
+++ test/simple_log_test.dart
@@ -1,6 +1,6 @@
 part of log4dart_test;
 
-// Copyright (c) 2012 Solvr, Inc. All rights reserved.
+// Copyright (c) 2013 Solvr, Inc. All rights reserved.
 //
 // This open source software is governed by the license terms
 // specified in the LICENSE file
